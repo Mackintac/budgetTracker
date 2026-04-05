240ddc4447431568fb385cacb20b808c1d650840
Mackintac <mack-robinson@hotmail.ca>
2026-04-05 10:27:13 -0400

    feat: add budget card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import SummaryCard from './components/SummaryCard';
 
 function App() {
-  return <div>Budget Tracker</div>;
+  return (
+    <div>
+      <h1>Budget Tracker</h1>
+      <SummaryCard label='Balance' amount={500} />
+      <SummaryCard label='Income' amount={500} />
+      <SummaryCard label='Expense' amount={500} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SummaryCard.jsx
@@ -0,0 +1,8 @@
+export default function SummaryCard({ label, amount }) {
+  return (
+    <div>
+      <p>{label}</p>
+      <p>{amount}</p>
+    </div>
+  );
+}
